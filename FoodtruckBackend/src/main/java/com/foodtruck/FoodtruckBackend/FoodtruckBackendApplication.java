4087efb4487e08d949274bf976909072be5b8a6a
uhas <drowssap123$>
2018-09-05 10:49:48 -0500

    inital commit. Spring Project.

---
+++ FoodtruckBackend/src/main/java/com/foodtruck/FoodtruckBackend/FoodtruckBackendApplication.java
@@ -0,0 +1,12 @@
+package com.foodtruck.FoodtruckBackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FoodtruckBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FoodtruckBackendApplication.class, args);
+	}
+}
---
+++ FoodtruckBackend/src/test/java/com/foodtruck/FoodtruckBackend/FoodtruckBackendApplicationTests.java
@@ -0,0 +1,16 @@
+package com.foodtruck.FoodtruckBackend;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class FoodtruckBackendApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
